48941f7a3cd986da146cd94b08240163285b137c
Hugo Nascimento <hugonsantos22@gmail.com>
2020-10-08 11:44:18 -0300

    Adicionado mensagem para trabalhar com projetos existentes

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Nivelamento Git e Github.");
+		System.out.println("Trabalhando com projeto existente.");
 	}
 
 }
